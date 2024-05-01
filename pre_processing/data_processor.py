0c59b7ecdf57e98a5f4932b06e7e160743841e6b
Shepherd <10481417+shepherd-06@users.noreply.github.com>
2024-05-02 00:19:49 +0300

    9 new dataset added about transmission from lit to other countries, data processor and key mapping file updated accordingly

--- pre_processing/data_processor.py
+++ pre_processing/data_processor.py
@@ -1,12 +1,12 @@
 import os
 import json
-from datetime import datetime
+import time
 from pre_processing.redis_con import RedisConnector
-from pre_processing.key_mappings import get_redis_key_base
+from pre_processing.key_mappings import get_redis_key_base, KEY_STRUCTURE
 
 
 class _DataProcessor:
-    def __init__(self, dataset_dir='../dataset', redis_connector=None):
+    def __init__(self, dataset_dir='dataset', redis_connector=None):
         self.dataset_dir = dataset_dir
         self.redis = redis_connector if redis_connector else RedisConnector().get_connection()
 
@@ -39,6 +39,7 @@ class _DataProcessor:
             if filename.endswith('.json'):
                 print("----------------------------------")
                 self._process_file(os.path.join(self.dataset_dir, filename))
+                time.sleep(2)  # sleep 2 second so I can read the log!
                 print("----------------------------------")
 
     def fetch_data_by_keytype(self, key_type):
@@ -52,9 +53,19 @@ class _DataProcessor:
         """
         return self.redis.hgetall(key_type)
 
+    def test(self):
+        for key in KEY_STRUCTURE:
+            data = self.fetch_data_by_keytype(KEY_STRUCTURE[key])
+            print(f"{key} - Total: {len(data)}")
 
-# if __name__ == "__main__":
-    # redis_connector = RedisConnector().get_connection()
-    # data_processor = _DataProcessor(redis_connector=redis_connector)
-    # data_processor.run()
-    # data_processor.fetch_data_by_keytype("wind_farms_production")
+
+if __name__ == "__main__":
+    redis_connector = RedisConnector().get_connection()
+    data_processor = _DataProcessor(redis_connector=redis_connector)
+    choice = input("1 to run and insert data on Redis, 2 to test > ")
+    if choice == "1":
+        data_processor.run()
+    elif choice == "2":
+        data_processor.test()
+    else:
+        print("Wrong Choice!")
--- pre_processing/key_mappings.py
+++ pre_processing/key_mappings.py
@@ -1,19 +1,19 @@
+"""
+A manually maintained dictionary that maps descriptive terms for datasets to their corresponding Redis key names and categories. 
+
+This structure is not dynamically linked to the categorization function `get_key_by_tags`. 
+Changes in this dictionary, such as adding new categories or changing descriptions, must be manually reflected in the `get_key_by_tags` function logic. Failure to update both can lead to mismatches and errors in key retrieval and categorization.
+
+Example:
+    KEY_STRUCTURE = {
+        'Actual National Electricity Consumption': 'electricity_consumption_actual', # consumption,
+        ...
+}
+
+This approach ensures that keys are organized by their function but requires careful maintenance 
+to keep the structure and the categorization logic in sync.
+"""
 KEY_STRUCTURE = {
-    """
-    A manually maintained dictionary that maps descriptive terms for datasets to their corresponding Redis key names and categories. 
-    
-    This structure is not dynamically linked to the categorization function `get_key_by_tags`. 
-    Changes in this dictionary, such as adding new categories or changing descriptions, must be manually reflected in the `get_key_by_tags` function logic. Failure to update both can lead to mismatches and errors in key retrieval and categorization.
-
-    Example:
-        KEY_STRUCTURE = {
-            'Actual National Electricity Consumption': 'electricity_consumption_actual', # consumption,
-            ...
-    }
-
-    This approach ensures that keys are organized by their function but requires careful maintenance 
-    to keep the structure and the categorization logic in sync.
-    """
     'Actual National Electricity Consumption': 'electricity_consumption_actual',  # consumption
     'Actual national electricity generation': 'electricity_generation',  # production
     'Actual national production of wind farms': 'wind_farms_production',  # source
@@ -24,7 +24,22 @@ KEY_STRUCTURE = {
     'Planned national electricity production': 'electricity_production_planned',  # production
     'Projected national electricity consumption': 'electricity_consumption_projected',  # consumption
     'Actual production of thermal power plants connected to PT': 'thermal_plants_production',  # source
-    'Actual national production of other energy sources': 'other_sources_production'  # source
+    'Actual national production of other energy sources': 'other_sources_production',  # source
+
+    # transmission - allocated (already allocated capacity aac)
+    'allocated latvia lithuania': "allocated_latvia_lithuania",
+    'allocated poland lithuania': 'allocated_poland_lithuania',  # transmission - allocated
+    'allocated sweden lithuania': 'allocated_sweden_lithuania',  # transmission - allocated
+    # transmission - atc (available transfer capacities)
+    'atc latvia lithuania': 'atc_latvia_lithuania',
+    'atc sweden lithuania': 'atc_sweden_lithuania',  # transmission - atc
+    'atc poland lithuania': 'atc_poland_lithuania',  # transmission - atc
+    # transmission - ntc (net transfer capacities)
+    'ntc latvia lithuania': 'ntc_latvia_lithuania',
+    'ntc sweden lithuania': 'ntc_sweden_lithuania',  # transmission - ntc
+    'ntc poland lithuania': 'ntc_poland_lithuania',  # transmission - ntc
+
+
 }
 
 
@@ -32,7 +47,8 @@ PLOT_TITLES_BY_CATEGORY = {
     'consumption': 'Electricity Consumption Trends',
     'production': 'Electricity Production Trends',
     'source': 'Electricity Source Distribution',
-    'storage': 'Electricity Storage Capacity Trends'
+    'storage': 'Electricity Storage Capacity Trends',
+    'transmission': 'Electricity Transmission To/From Other Countries',
 }
 
 
@@ -87,6 +103,21 @@ def get_key_by_tags(tag: str):
     elif tag == "storage":
         return {
             'Actual national production of storage devices': 'storage_devices_production',
+        }
+    elif tag == "transmission":
+        return {
+            # transmission - allocated (already allocated capacity aac)
+            'allocated latvia lithuania': "allocated_latvia_lithuania",
+            'allocated poland lithuania': 'allocated_poland_lithuania',  # transmission - allocated
+            'allocated sweden lithuania': 'allocated_sweden_lithuania',  # transmission - allocated
+            # transmission - atc (available transfer capacities)
+            'atc_latvia_lithuania': 'atc_latvia_lithuania',
+            'atc_sweden_lithuania': 'atc_sweden_lithuania',  # transmission - atc
+            'atc_poland_lithuania': 'atc_poland_lithuania',  # transmission - atc
+            # transmission - ntc (net transfer capacities)
+            'ntc_latvia_lithuania': 'ntc_latvia_lithuania',
+            'ntc_sweden_lithuania': 'ntc_sweden_lithuania',  # transmission - ntc
+            'ntc_poland_lithuania': 'ntc_poland_lithuania',  # transmission - ntc
         }
     else:
         # return error
